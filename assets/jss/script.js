f3d196e5fc54f729afb5748c1ddce24540af670d
Tiare <tmlincot@gmail.com>
2024-03-18 19:25:40 -0300

    Prueba completada

---
+++ assets/jss/script.js
@@ -0,0 +1,26 @@
+/* TOOLTIP */
+document.addEventListener("DOMContentLoaded", function() {
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+    tooltipTriggerList.forEach(function(tooltipTriggerEl) {
+        new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+});
+
+
+/* SMOTH SCROLL */
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelectorAll('.nav-link').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute('href').substring(1);
+            const targetSection = document.getElementById(targetId);
+
+            if (targetSection) {
+                targetSection.scrollIntoView({
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+});
